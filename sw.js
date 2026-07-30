25dda3735abf633a463f8dbf5f1f80753d43c0d7
eps-mignot <florianmignot@gmail.com>
2026-07-30 21:30:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,36 @@
-const CACHE_NAME = "jeu-lettres-cache-v1";
+const CACHE_NAME = "jeu-lettres-cache-v2";
 const ASSETS = [
+  "./",
   "index.html",
   "manifest.json",
-  "sw.js",
-  "Image/"
+  "img/icon-192.png",
+  "img/icon-512.png",
+  "Image/avion.png",
+  "Image/ballon.png",
+  "Image/chat.png",
+  "Image/dauphin.png",
+  "Image/elephant.png",
+  "Image/fleur.png",
+  "Image/guitare.png",
+  "Image/herisson.png",
+  "Image/igloo.png",
+  "Image/jaguar.png",
+  "Image/koala.png",
+  "Image/lapin.png",
+  "Image/maison.png",
+  "Image/nez.png",
+  "Image/orange.png",
+  "Image/poisson.png",
+  "Image/quille.png",
+  "Image/robot.png",
+  "Image/soleil.png",
+  "Image/tortue.png",
+  "Image/urne.png",
+  "Image/voiture.png",
+  "Image/wagon.png",
+  "Image/xylophone.png",
+  "Image/yaourt.png",
+  "Image/zebre.png"
 ];
 
 self.addEventListener("install", event => {
@@ -12,6 +39,19 @@ self.addEventListener("install", event => {
       return cache.addAll(ASSETS);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
